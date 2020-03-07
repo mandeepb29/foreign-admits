5da12bebfce1239e2c2124671bf7056063fc3161
mandeepb29 <mandeepb29@gmail.com>
2020-03-08 02:41:13 +0530

    update

--- src/app/admin-panels/student-admin/student-admin.module.ts
+++ src/app/admin-panels/student-admin/student-admin.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentAdminComponent } from './student-admin.component';
  import { SDashboardComponent } from './s-dashboard/s-dashboard.component';
-// import { AdminSidebarModule } from './admin-sidebar/admin-sidebar.module';
-// import { AdminHeaderModule } from './admin-header/admin-header.module';
 import { StudentAdminRoutingModule } from './student-admin-routing.module';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { AdminSidebarComponent } from './admin-sidebar/admin-sidebar.component';
